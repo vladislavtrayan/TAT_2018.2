ad3fce269c4f25ea94534ba35b76de8ff302138a
vladislav.trayan <vladislav.trayan@gmail.com>
2018-10-19 21:02:03 +0300

    Update Program.cs

---
+++ Dev-1/Dev-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace Dev_1
+{
+    class FinderOfMaximumSubsequenceOfUniqueElements
+    {
+        /// <summary>
+        /// Entry point of the program
+        /// </summary>
+        /// <param name="args"></param>
+        
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No arguments"); // checking if there is no arguments 
+                return 0;
+            }
+
+            StringBuilder initialLineOfElements = new StringBuilder(); 
+            for (int  i = 0; i < args.Length; ++i)
+            {
+                initialLineOfElements.Append(args[i]); // if there more then one argument - sum all the arguments into one string
+            }
+
+            LenghtOfMaxSubsequence classObject = new LenghtOfMaxSubsequence(initialLineOfElements);
+            Console.WriteLine(classObject.SearchMaximumLenghtOfUniqueSequence());
+            return 0;
+        }
+    }
+}
