466be10ab385457fabce1f3d01b401d14d11938d
vladislav.trayan <vladislav.trayan@gmail.com>
2018-11-02 21:42:28 +0300

    Update Node.cs

--- Dev-4/Dev-4/Node.cs
+++ Dev-4/Dev-4/Node.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Dev_4
 {
+    /// <summary>
+    /// keeps the tag and all information that it contain
+    /// </summary>
     class Node
     {
         public bool Flag { get; set; }
@@ -14,23 +13,41 @@ namespace Dev_4
         public string information { get; set; }
         Dictionary<string,string> additionalProperties;
         List<Node> includedList;
+        /// <summary>
+        /// class constructor
+        /// </summary>
         public Node ()
         {
             additionalProperties = new Dictionary<string, string>();
             includedList = new List<Node>();
         }
+        /// <summary>
+        /// add new element to additional properties of each tag
+        /// fill it as a dicitonary with string value and string property
+        /// </summary>
         public void AddProperties (string property,string value)
         {
             additionalProperties.Add(property, value);
         }
+        /// <summary>
+        /// method for adding new Node to includedList
+        /// </summary>
+        /// <param name="node"></param>
         public void AddNewIncludedList (Node node)
         {
             includedList.Add(node);
         }
+        /// <summary>
+        /// method for adding new List<Node> to includedList
+        /// </summary>
+        /// <param name="newList"></param>
         public void AddNewIncludedList(List<Node> newList)
         {
             includedList.AddRange(newList);
         }
+        /// <summary>
+        /// .....
+        /// </summary>
         public void OutPut ()
         {
         }
