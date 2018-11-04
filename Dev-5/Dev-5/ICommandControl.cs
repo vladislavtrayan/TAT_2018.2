7acecdf36da29ae30c04c0c8897f8289596e304a
vladislav.trayan <vladislav.trayan@gmail.com>
2018-11-04 14:18:34 +0300

    ICommandControl.cs

---
+++ Dev-5/Dev-5/ICommandControl.cs
@@ -0,0 +1,14 @@
+﻿namespace Dev_5
+{
+    /// <summary>
+    /// interface containing methods with commands
+    /// </summary>
+    interface ICommandControl
+    {
+        void AddNewCar();
+        void ShowAveragePrice();
+        void ShowAveragePriceByType();
+        void ShowCapacity();
+        void ShowNumberOfTypes();
+    }
+}
