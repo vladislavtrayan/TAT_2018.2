ff47d4fb55e88dfec85caeb7338de4bbebcef2da
vladislav.trayan <vladislav.trayan@gmail.com>
2018-11-11 16:30:55 +0300

    Add ListOutputter.cs

---
+++ Dev-4/Dev-4/ListOutputter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dev_4
+{
+    class ListOutputter
+    {
+        public void Output(List<string[]> listOfString)
+        {
+            foreach (string[] i in listOfString)
+            {
+                for (int j = 0; j < i.Length - 1;j++)
+                {
+                    Console.Write(i[j] + " -> ");
+                }
+                Console.WriteLine(i[i.Length - 1]);
+            }
+        }
+    }
+}
