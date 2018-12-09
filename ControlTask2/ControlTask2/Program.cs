02a1cdf17d911a10205814ff1fe0664847a238a2
vladislav.trayan <vladislav.trayan@gmail.com>
2018-12-09 14:56:45 +0300

    Add files

---
+++ ControlTask2/ControlTask2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ControlTask2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            UrlFinder urlFinder = new UrlFinder();
+            string url = urlFinder.FindUrl("http://google.com");
+            Console.WriteLine(url);
+        
+    }
+}
