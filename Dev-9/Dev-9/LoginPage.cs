0d8011467986749ebe30864026b3cade4cf041b6
vladislav.trayan <vladislav.trayan@gmail.com>
2018-12-11 18:46:56 +0300

    Divide project into different files

---
+++ Dev-9/Dev-9/LoginPage.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+
+namespace Dev9
+{
+    /// <summary>
+    /// Operates with login page
+    /// </summary>
+    public class LoginPage
+    {
+        IWebDriver driver;
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        /// <summary>
+        /// Enter data into textboxes
+        /// Press login button to enter service 
+        /// </summary>
+        public void Login ()
+        {
+            IWebElement emailTextBox = driver.FindElement(By.Id("email"));
+            IWebElement passWordTextBox = driver.FindElement(By.Id("pass"));
+            IWebElement loginButton = driver.FindElement(By.Id("login_button"));
+
+            emailTextBox.Clear();
+            passWordTextBox.Clear();
+
+            emailTextBox.SendKeys(Email);
+            passWordTextBox.SendKeys(Password);
+            loginButton.Click();
+        }
+    }
+}
