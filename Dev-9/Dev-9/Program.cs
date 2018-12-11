fb73de9b60084f0d81f9f34f413c80cac94d8c9e
vladislav.trayan <vladislav.trayan@gmail.com>
2018-12-11 18:46:56 +0300

    Divide project into different files

--- Dev-9/Dev-9/Program.cs
+++ Dev-9/Dev-9/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using NUnit.Framework;
+using System.Collections.Generic;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System.Threading;
 
 
 namespace Dev9
@@ -25,74 +24,20 @@ namespace Dev9
             password = "password";
 
             driver.Url = "https://vk.com/login";
-            for (int i = 0; i < 100; i++)
-            {
-                Thread.Sleep(100);
-                try
-                {
-                    if (driver.FindElement(By.Id("login_button")).Displayed)
-                    {
-                        break;
-                    }
-                }
-                catch
-                {
-                    continue;
-                }
-            }
             driver.Manage().Window.Maximize();
 
-            IWebElement emailTextBox = driver.FindElement(By.Id("email"));
-            IWebElement passWordTextBox = driver.FindElement(By.Id("pass"));
-            IWebElement loginButton = driver.FindElement(By.Id("login_button"));
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.Email = email;
+            loginPage.Password = password;
+            loginPage.Login();
 
-            emailTextBox.Clear();
-            passWordTextBox.Clear();
+            MessagePage messagePage = new MessagePage(driver);
+            messagePage.SwitchToMessagePage();
+            messagePage.SelectUnreadMessages();
+            messagePage.FindMessages();
 
-            emailTextBox.SendKeys(email);
-            passWordTextBox.SendKeys(password);
-            loginButton.Click();
+            List<IWebElement> messages = new List<IWebElement>();
 
-            for (int i = 0; i < 1000; i++)
-            {
-                Thread.Sleep(100);
-                try
-                {
-                    if (driver.FindElement(By.Id("side_bar")).Displayed)
-                    {
-                        break;
-                    }
-                }
-                catch
-                {
-                    continue;
-                }
-            }
-
-            IWebElement messageButton = driver.FindElement(By.Id("l_msg"));
-            messageButton.Click();
-
-            for (int i = 0; i < 1000; i++)
-            {
-                Thread.Sleep(100);
-                try
-                {
-                    if (driver.FindElement(By.Id("ui_rmenu_unread")).Displayed)
-                    {
-                        break;
-                    }
-                }
-                catch
-                {
-                    continue;
-                }
-            }
-
-            IWebElement unreadMesseges = driver.FindElement(By.Id("ui_rmenu_unread"));
-            unreadMesseges.Click();
-
-            IWebElement dialogElements = driver.FindElement(By.Id("im_dialogs"));
-            var messages = dialogElements.FindElements(By.ClassName("nim-dialog--cw"));
             foreach (var i in messages)
             {
                 Console.WriteLine("From : " + i.FindElement(By.ClassName("nim-dialog--name")).Text);
